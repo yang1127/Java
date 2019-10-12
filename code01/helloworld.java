f30b33de82a5f41c9aa9b0bf163e31d5d383f1c2
yang1127 <1536520643@qq.com>
2019-10-12 21:46:06 +0800

    code01

---
+++ code01/helloworld.java
@@ -0,0 +1,40 @@
+package code01;
+
+public class helloworld {
+	public static void main(String[] args){  //main()声明
+		//hello world
+		//System.out.println("hello world");
+		
+		//System中S大写，输出：System.out.println
+		
+		//Welcome to java
+		/*System.out.println("*****************");
+		System.out.println("*Welcome to Java*");
+		System.out.println("*****************");*/
+		
+		//打印命令行参数——进行Arguments设置
+		/*System.out.println("*打印命令行参数*");
+		
+		for (int i = 0; i < args.length; i++)
+		{
+			System.out.println(args[i]);
+		}*/
+			
+		//水仙花数
+		/*int i, j, k;
+		for (int num = 100; num <= 999; num++)
+		{
+			i = num % 10;
+			j = num / 10 % 10;
+			k = num / 100;
+			
+			if (i * i * i + j * j * j + k * k * k == num)
+			{
+				System.out.println(num + "\t");
+			}
+		}
+		
+		System.out.println();*/
+			
+	}
+}
