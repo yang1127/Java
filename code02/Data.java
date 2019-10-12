d2c6715bf64535f63e1f3534f8f5e782e69b9ea3
yang1127 <1536520643@qq.com>
2019-10-12 22:11:10 +0800

    code02-data

---
+++ code02/Data.java
@@ -0,0 +1,39 @@
+package code02;
+
+public class Data {
+	public static void main(String[] args){
+		/*int num = 5;
+		System.out.println(num);
+		
+		float floatnum = 5.99f; 
+		System.out.println(floatnum);
+		
+		char Letter = 'D';
+		System.out.println(Letter);
+		
+		boolean Bool = true;
+		System.out.println(Bool);
+		
+		String Text = "Hello"; 
+		System.out.println(Text);
+		
+		byte Num = 100;
+		System.out.println(Num);
+		
+		short myNum = 5000; 
+		System.out.println(myNum);
+		
+		long Num1 = 15000000000L; 
+		System.out.println(Num1);
+		
+		float Num2 = 5.75f; 
+		System.out.println(Num2);*/
+		
+		double myDouble = 9.78; 
+		int myInt = (int) myDouble; 
+		
+		System.out.println(myInt); // 9
+		System.out.println(myDouble); //9.78
+	}
+
+}
