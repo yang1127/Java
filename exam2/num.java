6fa71d90793a6ced7acdd7f8235a1a0e4232140f
yang1127 <1536520643@qq.com>
2019-12-16 23:40:07 +0800

    test

---
+++ exam2/num.java
@@ -0,0 +1,41 @@
+package exam2;
+
+public class num {
+	public static int romanToInt(String s)
+	{
+		char[] ch = s.toCharArray();
+		int result = 0;
+		for(int i = 0; i < ch.length; ++i)
+		{
+			switch(ch[i]) {
+			case 'M':result += 1000; break;
+			case 'D':result += 500; break;
+			case 'L':result += 50; break;
+			case 'V':result += 5; break;
+			case 'I':
+				if(i + 1 < ch.length && (ch[i + 1] == 'V' || ch[i + 1] == 'X' )) {
+					result -= 1;
+				}else {
+					result += 1;
+				}
+			
+			case 'X':
+				if(i + 1 < ch.length && (ch[i + 1] == 'L' || ch[i + 1] == 'C' )) {
+					result -= 10;
+				}else {
+					result += 10;
+				}
+			
+			case 'C':
+				if(i + 1 < ch.length && (ch[i + 1] == 'D' || ch[i + 1] == 'M' )) {
+					result -= 100;
+				}else {
+					result += 100;
+				}
+				break;
+			
+			}
+		}
+		return result;
+	}
+}
