2c84eeba4a7005910d07cdaec872d2baec33143f
yang1127 <1536520643@qq.com>
2019-12-29 22:15:54 +0800

    期末练习_进制转换

---
+++ transform/test.java
@@ -0,0 +1,10 @@
+package transform;
+
+public class test {
+	public static void main(String[] args) {
+        int a = -1234567;
+        System.out.println("二进制："+Integer.toBinaryString(a));
+        System.out.println("八进制："+Integer.toOctalString(a));
+        System.out.println("十六进制："+Integer.toHexString(a));
+    }
+}
